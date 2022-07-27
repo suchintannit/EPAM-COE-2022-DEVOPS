5291df5636973d6b165962f3f793ed5da93ef6fc
Suchintan Mishra <suchintan.mishra@gmail.com>
2022-07-28 04:54:38 +0530

    Add files via upload

---
+++ kubernetes-vagrant-ansible-ubuntu/deploy-master.sh
@@ -0,0 +1,12 @@
+################################# ABOUT ############################################
+################################# Suchintan Mishra #################################   #
+# This is a fully automated script that must be executed on all master and all workers.#
+# Installs all the required dependencies for the running of kubernetes and docker.     #
+####################################################################################   #
+#!/bin/bash
+echo "#########################common"
+sudo sudo rm -rf /var/lib/apt/lists/*
+sudo sudo apt-get clean
+sudo -i apt-get update -y
+sudo -i apt-get install ansible -y
+ansible-playbook master-playbook.yml -vvvvvv
---
+++ kubernetes-vagrant-ansible-ubuntu/deploy-node.sh
@@ -0,0 +1,10 @@
+################################# ABOUT ############################################
+################################# Suchintan Mishra #################################   #
+# This is a fully automated script that must be executed on all master and all workers.#
+# Installs all the required dependencies for the running of kubernetes and docker.     #
+####################################################################################   #
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install ansible -y
+ansible-playbook node-playbook.yml -vvvv
