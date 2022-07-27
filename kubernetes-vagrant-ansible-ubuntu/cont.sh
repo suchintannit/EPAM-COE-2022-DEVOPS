56a9edddfda24aafc30210d5eae7f9d34791b8c7
Suchintan Mishra <suchintan.mishra@gmail.com>
2022-07-28 04:54:08 +0530

    Delete cont.sh

--- kubernetes-vagrant-ansible-ubuntu/cont.sh
+++
@@ -1,9 +0,0 @@
-################################# ABOUT ############################################
-################################# Suchintan Mishra #################################   #
-# This is a fully automated script that must be executed on all master and all workers.#
-# Installs all the required dependencies for the running of kubernetes and docker.     #
-####################################################################################   #
-#!/bin/bash
-
-sudo rm /etc/containerd/config.toml
-sudo -i systemctl restart containerd
